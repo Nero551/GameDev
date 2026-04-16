cf731fe42d37ad91d0580c5a7b3b76c93b5480a4
Nero551 <109078102+Nero551@users.noreply.github.com>
2026-04-16 06:39:02 +0200

    slowly starting the the change to godot cuz roblox keeps making bad decisions

---
+++ Projects/Learning C#/test.cs
@@ -0,0 +1,21 @@
+using System.Reflection.Metadata.Ecma335;
+
+int num = 5;
+if (num > 5)
+{
+    num = 6;
+Console.WriteLine(num);
+
+}
+class value {}
+
+namespace Test
+{
+
+    class program
+    {
+        lol();
+        
+    }
+    
+}
