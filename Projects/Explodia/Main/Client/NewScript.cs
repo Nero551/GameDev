88d91111690b8c031c6691a55580c0df327acd0d
Nero551 <109078102+Nero551@users.noreply.github.com>
2026-04-16 11:41:40 +0200

    actually starting to get the hang of OOP

--- Projects/Explodia/Main/Client/NewScript.cs
+++ Projects/Explodia/Main/Client/NewScript.cs
@@ -1,10 +1,12 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 
-public partial class NewScript : Node
+public partial class StateManager : Node
 {
+	public Dictionary<string,double> ActiveStates  = new();
 
 	// Called when the node enters the scene tree for the first time.
 	public void Test()
--- Projects/Explodia/Main/Client/PlayerController.cs
+++ Projects/Explodia/Main/Client/PlayerController.cs
@@ -1,19 +1,25 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public partial class PlayerController : Node
 {
-
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		var func = GetNode<NewScript>("Node3D");
-		func.Test();
+		var ol = new List<int> { 1, 2, 5, 6 };
+		var dic = new Dictionary<string, double>
+		{
+			{"A" , 5},
+			{"B", 7.5}
+		};
+		var obj = GetNode<StateManager>("Node3D");
+		obj.ActiveStates["Stunned"] = 4.5;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-
+		 StateManager lol = new  StateManager();
 	}
 }
