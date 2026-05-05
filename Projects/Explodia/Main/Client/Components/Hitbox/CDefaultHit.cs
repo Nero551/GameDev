4217359cd069025de87ac6bb8ced45afa5a4b29a
Nero551 <109078102+Nero551@users.noreply.github.com>
2026-05-05 17:01:01 +0300

    finished the move to the new framework EIC , gonna take a break and work in the lab project some more

--- Projects/Explodia/Main/Client/Components/Hitbox/CDefaultHit.cs
+++ Projects/Explodia/Main/Client/Components/Hitbox/CDefaultHit.cs
@@ -3,6 +3,8 @@ using System;
 
 public partial class CDefaultHit : Component
 {
+    
+
     public void DefaultHit(ECharacter Attacker, ECharacter targetHit, Godot.Collections.Dictionary itemData)
     {
         if (targetHit.Cstates.CheckState("Invulnerable"))
@@ -14,7 +16,7 @@ public partial class CDefaultHit : Component
         targetHit.Cstates.AddState("Stunned", 0.2);
         //Damage
         targetHit.CurrentHealth -= (float)itemData["Damage"];
-        Mathf.Max(0, targetHit.CurrentHealth);
+        targetHit.CurrentHealth = Mathf.Max(0, targetHit.CurrentHealth);
 
         targetHit.Canimations.PlayAnim("HitReactions/" + Attacker.Ccombat.SwingNumber, 1);
 
