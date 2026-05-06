462d53c19f98afc94a49fe10375ce347e4422436
Nero551 <109078102+Nero551@users.noreply.github.com>
2026-05-06 05:42:25 +0300

    i dont even know wut am doing anymore.

--- Projects/Lab/Main/2D/Coordinate Plane/Grid2D.cs
+++ Projects/Lab/Main/2D/Coordinate Plane/Grid2D.cs
@@ -10,31 +10,35 @@ public partial class Grid2D : Node
     public override void _Ready()
     {
         CreateGrid(20, 20);
+        CreateLine(Origin, new Vector2(0, 4));
+        CreateLine(new Vector2(0, 4), new Vector2(3, 0));
     }
+
+    //this will be a game where u can move between points in the grid and make lines real-time.
 
     public void CreateGrid(int sizeX, int sizeY)
     {
-        CreatePoint(Origin);
+        CreatePoint(Origin, Colors.Green);
 
-        for (int i = 1; i <= sizeX; i += BasisX)
+        for (int x = -sizeX; x <= sizeX; x += BasisX)
         {
-            CreatePoint(new Vector2(i, 0));
-            CreatePoint(new Vector2(-i, 0));
-        }
-
-        for (int i = 1; i <= sizeY; i += BasisY)
-        {
-            CreatePoint(new Vector2(0, i));
-            CreatePoint(new Vector2(0, -i));
+            for (int y = -sizeY; y <= sizeY; y += BasisY)
+            {
+                if (x == 0 && y == 0)
+                {
+                    continue;
+                }
+                CreatePoint(new Vector2(x, y));
+            }
         }
     }
 
-    public Point2D CreatePoint(Vector2 pos, Color color = default)
+    public Node2D CreatePoint(Vector2 pos, Color color = default)
     {
         PackedScene scene = GD.Load<PackedScene>("res://Main/2D/Coordinate Plane/Scenes/point.tscn");
-        Point2D point = scene.Instantiate<Point2D>();
+        Node2D point = scene.Instantiate<Node2D>();
 
-        point.Position = pos;
+        point.Position = new Vector2(pos.X, pos.Y * -1);
 
         point.Name = pos.ToString();
 
@@ -44,5 +48,26 @@ public partial class Grid2D : Node
         AddChild(point);
 
         return point;
+    }
+
+    public void CreateLine(Vector2 a, Vector2 b, Color color = default)
+    {
+        PackedScene scene = GD.Load<PackedScene>("res://Main/2D/Coordinate Plane/Scenes/line.tscn");
+        Node2D line = scene.Instantiate<Node2D>();
+
+        var meshInstance = line.GetNode<MeshInstance2D>("MeshInstance2D");
+        var cylinder = meshInstance.Mesh as CapsuleMesh;
+
+        line.Position = ((new Vector2(a.X, a.Y * -1) + new Vector2(b.X, b.Y * -1))) / 2;
+        cylinder.Height = (b - a).Length() * 4;
+        line.Rotation = (b - a).Angle();
+        GD.Print(Mathf.RadToDeg(line.Rotation));
+
+        line.Name = (a + b).ToString();
+
+        meshInstance.Modulate = color == default ? Colors.Black : color;
+
+
+        AddChild(line);
     }
 }
