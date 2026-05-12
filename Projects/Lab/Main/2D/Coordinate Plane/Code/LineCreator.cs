b59001a6ff3f830699cc91df24eaaed3047a2f8b
Nero551 <109078102+Nero551@users.noreply.github.com>
2026-05-12 17:06:26 +0300

    made rays work too

--- Projects/Lab/Main/2D/Coordinate Plane/Code/LineCreator.cs
+++ Projects/Lab/Main/2D/Coordinate Plane/Code/LineCreator.cs
@@ -84,7 +84,26 @@ public partial class CartesianPlane
 
     public void Ray(Vector2 a, Vector2 b, Color color = default)
     {
+        PackedScene scene = GD.Load<PackedScene>("res://Main/2D/Coordinate Plane/Scenes/Ray.tscn");
+        Node2D line = scene.Instantiate<Node2D>();
+        var meshInstance = line.GetNode<MeshInstance2D>("Line/LineMesh");
+        var arrow = line.GetNode<MeshInstance2D>("Arrow/ArrowMesh");
 
+        a = new Vector2(a.X * BasisX, -a.Y * BasisY);
+        b = new Vector2(b.X * BasisX, -b.Y * BasisY);
+        Vector2 vAB = b - a;
+
+        line.Position = a;
+        line.GetNode<Node2D>("Line").Scale = new Vector2(vAB.Length(), 0.5f);
+        line.Rotation = (b - a).Angle();
+
+        line.GetNode<Node2D>("Arrow").Position = new Vector2(line.GetNode<Node2D>("Line").Scale.X, 0);
+
+        line.Name = (a + b).ToString();
+        meshInstance.Modulate = color == default ? Colors.DimGray : color;
+        arrow.Modulate = color == default ? Colors.DimGray : color;
+
+        GetNode<Node2D>("My Stuff").AddChild(line);
     }
 
     //TODO- straight lines or anything with arrows dont work
@@ -104,8 +123,8 @@ public partial class CartesianPlane
         line.GetNode<Node2D>("Line").Scale = new Vector2(Size * 2, 0.5f);
         line.Rotation = (b - a).Angle();
 
-        line.GetNode<Node2D>("Arrow1").Position = new Vector2(line.GetNode<Node2D>("Line").Scale.X / 2, 0);
-        line.GetNode<Node2D>("Arrow2").Position = new Vector2(-line.GetNode<Node2D>("Line").Scale.X / 2, 0);
+        line.GetNode<Node2D>("Arrow1").Position = new Vector2(-line.GetNode<Node2D>("Line").Scale.X / 2, 0);
+        line.GetNode<Node2D>("Arrow2").Position = new Vector2(line.GetNode<Node2D>("Line").Scale.X / 2, 0);
 
         line.Name = (a + b).ToString();
         meshInstance.Modulate = color == default ? Colors.DimGray : color;
--- Projects/Lab/Main/2D/Coordinate Plane/Code/Sandbox.cs
+++ Projects/Lab/Main/2D/Coordinate Plane/Code/Sandbox.cs
@@ -9,7 +9,12 @@ public partial class CartesianPlane
         // LineSegment(Origin, new Vector2(3,4), Colors.Crimson);
         // LineSegment(Origin, new Vector2(3, 0), Colors.DeepSkyBlue);
         // LineSegment(new Vector2(3, 0), new Vector2(3, 4), Colors.DarkGreen);
-        StraightLine(new Vector2(0,5), new Vector2(5,0), Colors.White);
+        StraightLine(new Vector2(0, 5), new Vector2(5, 0), Colors.White);
+        Ray(Origin, new Vector2(5, 5), Colors.Pink);
+        Ray(Origin, new Vector2(-5, -5), Colors.Pink);
+        Ray(Origin, new Vector2(-5, 5), Colors.Pink);
+        Ray(Origin, new Vector2(5, -5), Colors.Pink);
+        Ray(new Vector2(-5, -5), new Vector2(-7, 5), Colors.Pink);
 
         // LineSegment(new Vector2(5,5), new Vector2(10,10),Colors.Beige);
         // StraightLine(Origin, new Vector2(30, -25), Colors.Blue);
