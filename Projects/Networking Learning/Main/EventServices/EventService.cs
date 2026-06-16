a86a04c1a30eb475144d3c282206fadc0c86f607
Nero <109078102+Nero551@users.noreply.github.com>
2026-06-16 10:26:59 +0300

    unsubscribing from events didnot work , fixed it. also made it possible for many methods to sub from 1 line

--- Projects/Networking Learning/Main/EventServices/EventService.cs
+++ Projects/Networking Learning/Main/EventServices/EventService.cs
@@ -2,14 +2,10 @@ using System;
 using System.Collections.Generic;
 using Godot;
 
-public class EventService
+public static class EventService
 {
-    public static Dictionary<string, Delegate> Events = [];
-    //TODO- need to make EventService since i realized i am starting to need C# events to replace signals.
-    /*
-        to make this eventservice i need to learn delegates and events. atleast that what i know i need to learn.
-    */
-
+    private static Dictionary<string, Delegate> Events = [];
+    
     public static void Fire<T>(T @event) where T : Event
     {
         if (Events.TryGetValue(@event.GetType().Name, out Delegate callback))
@@ -18,24 +14,32 @@ public class EventService
         }
     }
 
-    public static void Subscribe<T>(Action<T> callback) where T : Event
+    public static void Subscribe<T>(params Action<T>[] callbacks) where T : Event
     {
-        if (Events.ContainsKey(typeof(T).Name))
+        foreach (Action<T> callback in callbacks)
         {
-            if (Events[typeof(T).Name] != null)
+            if (Events.ContainsKey(typeof(T).Name))
             {
-                Events[typeof(T).Name] = Delegate.Combine(callback, Events[typeof(T).Name]);
-                return;
+                if (Events[typeof(T).Name] != null)
+                {
+                    Events[typeof(T).Name] = Delegate.Combine(callback, Events[typeof(T).Name]);
+                }
+            }
+            else
+            {
+                Events[typeof(T).Name] = callback;
             }
         }
-        Events[typeof(T).Name] = callback;
     }
 
-    public static void Unsubscribe<T>(Action<T> callback) where T : Event
+    public static void Unsubscribe<T>(params Action<T>[] callbacks) where T : Event
     {
-        if (Events.ContainsValue(callback))
+        foreach (Action<T> callback in callbacks)
         {
-            Events[typeof(T).Name] = Delegate.Remove(Events[typeof(T).Name], callback);
+            if (Events.ContainsKey(typeof(T).Name))
+            {
+                Events[typeof(T).Name] = Delegate.Remove(Events[typeof(T).Name], callback);
+            }
         }
     }
 }
--- Projects/Networking Learning/Main/Game.cs
+++ Projects/Networking Learning/Main/Game.cs
@@ -13,16 +13,20 @@ public partial class Game : Node3D
     public override void _Ready()
     {
         base._Ready();
-        EventService.Subscribe<Events.ClientConnected>(TestMethod);
+        EventService.Subscribe<Events.ClientConnected>(TestMethod, Test2Method);
         EventService.Fire<Events.ClientConnected>(new Events.ClientConnected(5));
-
+		EventService.Unsubscribe<Events.ClientConnected>(TestMethod,Test2Method);
+        EventService.Fire<Events.ClientConnected>(new Events.ClientConnected(6));
     }
 
     void TestMethod(Events.ClientConnected @event)
     {
         GD.Print(@event.ClientId);
+    }
+
+    void Test2Method(Events.ClientConnected @event)
+    {
         GD.Print("Nice");
-        // GD.Print(clientConnected.ClientId);
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
